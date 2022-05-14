c5dd63668c3e063365c0d7b2d33107577578cf5d
Persie0 <hashcod3@protonmail.com>
2022-05-14 22:55:15 +0200

    highlighting the word in the example sentences

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def init_param():
     fromLang = sys.argv[2]
     toLang = sys.argv[3]
     # fix as \ in created workflow file just disappears - so made 2
-    path = sys.argv[1].replace("\\\\", "\\")
+    path = sys.argv[1].replace("\\", "",0)
     filename = ntpath.basename(path)
     numberOfWords = int(sys.argv[4])
     if not filename.endswith(".txt"):
